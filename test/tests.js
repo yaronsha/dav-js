3f96b95e88194285367162e515f4b38a8883f648
yaronsha <yaron.shapira7@gmail.com>
2018-06-25 15:55:35 +0300

    update tests

---
+++ test/tests.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const mocha = require('mocha');
+
+mocha.describe ('test', function() {
+  mocha.it('should complete this test', function () {
+    assert.ok(true);
+  });
+});
+
+
